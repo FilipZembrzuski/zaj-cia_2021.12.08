e89a9f6e136e8a60ae179e3c823ab741877749cb
FilipZembrzuski <fzembrzuski@gmail.com>
2021-12-08 10:32:23 +0100

    dodanie pliku php i coś w html

---
+++ sprawdz.php
@@ -0,0 +1,15 @@
+<?php
+
+$stanp = $_GET['stan'];
+if($stanp==1){
+    echo("Zmienna: ".$stanp);
+}
+else if($stanp==2){
+    echo("zmienna inna: ".$stanp);
+}
+else{
+    echo "nie wiem co u było bo nie zdążyłem";
+}
+
+
+?>
